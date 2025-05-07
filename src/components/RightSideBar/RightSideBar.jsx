c5fba82d846b2bbba90a40d7f0ba498db36c64db
Ahmed Ebeed <aebeedcs@gmail.com>
2025-05-07 06:23:53 +0300

    MOD

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './RightSideBar.css'
+import assets from '../../assets/assets'
 const RightSideBar = () => {
     return (
-        <div>
-
+        <div className='rs'>
+            <div className="rs-profile">
+                <img src={assets.profile_img} alt="" />
+                <h3>Richard sanford <img src={assets.green_dot} className='dot' alt="" /></h3>
+                <p>Hey, There i am Richard stanford using chat app</p>
+            </div>
+            <hr />
+            <div className="rs-media">
+                <p>media</p>
+                <div>
+                    <img src={assets.pic1} alt="" />
+                    <img src={assets.pic2} alt="" />
+                    <img src={assets.pic3} alt="" />
+                    <img src={assets.pic4} alt="" />
+                    <img src={assets.pic1} alt="" />
+                    <img src={assets.pic2} alt="" />
+                </div>
+            </div>
+            <button>logout</button>
         </div>
     )
 }
